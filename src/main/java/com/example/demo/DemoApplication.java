de48d5c8fa27b2d4de6828e2b9f2ca685721dd62
Dave Syer <dsyer@vmware.com>
2022-12-07 15:49:41 -0800

    gRPC in a native image, yay!

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.aot.hint.ExecutableMode;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.util.ReflectionUtils;
+
+import io.netty.buffer.AbstractByteBufAllocator;
+
+@SpringBootApplication
+@ImportRuntimeHints(DemoApplicationRuntimeHints.class)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+class DemoApplicationRuntimeHints implements RuntimeHintsRegistrar {
+
+    @Override
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        hints.reflection().registerType(AbstractByteBufAllocator.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS);
+    }
+
+}
---
+++ src/main/java/com/example/demo/GrpcServerService.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import com.example.demo.proto.HelloReply;
+import com.example.demo.proto.HelloRequest;
+import com.example.demo.proto.SimpleGrpc;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GrpcServerService extends SimpleGrpc.SimpleImplBase {
+
+    @Override
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+
+}

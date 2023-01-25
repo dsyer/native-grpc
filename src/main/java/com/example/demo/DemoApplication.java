4a9aa87e8f5c08cff8109160a1fbd90d93d47ac8
Dave Syer <dsyer@vmware.com>
2023-01-25 12:37:45 +0000

    Add back native image support

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+import io.netty.buffer.AbstractByteBufAllocator;
 
 @SpringBootApplication
+@ImportRuntimeHints(DemoApplicationRuntimeHints.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+class DemoApplicationRuntimeHints implements RuntimeHintsRegistrar {
+
+	@Override
+	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+		hints.reflection().registerType(AbstractByteBufAllocator.class, MemberCategory.INVOKE_DECLARED_METHODS);
 	}
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,25 +2,38 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationListener;
 
 import com.example.demo.proto.HelloReply;
 import com.example.demo.proto.HelloRequest;
 import com.example.demo.proto.SimpleGrpc;
 
-import io.grpc.Grpc;
-import io.grpc.InsecureChannelCredentials;
 import io.grpc.Server;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import net.devh.boot.grpc.client.inject.GrpcClientBean;
 import net.devh.boot.grpc.server.event.GrpcServerStartedEvent;
 
-@SpringBootTest
-// @ExtendWith(GrpcCleanupExtension.class)
+@SpringBootTest(properties = { "grpc.client.test.address=static://localhost:9090",
+		"grpc.client.test.negotiationType=plaintext", "debug=true" })
+@AutoConfigureObservability
 class DemoApplicationTests {
 
-	// @Autowired
-	private TestListener listener;
+	private static Log log = LogFactory.getLog(DemoApplicationTests.class);
+
+	@Autowired
+	private SimpleGrpc.SimpleBlockingStub stub;
+
+	@Autowired
+	private ObservationRegistry observations;
 
 	@Test
 	void contextLoads() {
@@ -28,14 +41,16 @@ class DemoApplicationTests {
 
 	@Test
 	void serverResponds() {
-		var channel = Grpc.newChannelBuilderForAddress("0.0.0.0", 9090, InsecureChannelCredentials.create())
-				.build();
-		var stub = SimpleGrpc.newBlockingStub(channel);
-		HelloReply response = stub.sayHello(HelloRequest.newBuilder().setName("Alien").build());
-		assertEquals("Hello ==> Alien", response.getMessage());
+		Observation observation = Observation.createNotStarted("test", this.observations);
+		observation.observe(() -> {
+			log.info("Testing");
+			HelloReply response = stub.sayHello(HelloRequest.newBuilder().setName("Alien").build());
+			assertEquals("Hello ==> Alien", response.getMessage());
+		});
 	}
 
-	// @TestConfiguration
+	@TestConfiguration
+	@GrpcClientBean(clazz = SimpleGrpc.SimpleBlockingStub.class, beanName = "stub", client = @GrpcClient("test"))
 	static class TestListener implements ApplicationListener<GrpcServerStartedEvent> {
 
 		private Server server;
